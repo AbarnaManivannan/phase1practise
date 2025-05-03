7b41053314be64d241f4f420c46fc892d851661b
AbarnaManivannan <madhu.vravi@yahoo.com>
2025-05-03 16:52:03 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 echo "Running start_server.sh"
+
+# Run the JAR with nohup and log output to a file
 nohup java -jar /home/ec2-user/app/hello-world-1.0-SNAPSHOT-jar-with-dependencies.jar > /home/ec2-user/app/output.log 2>&1 &
+
+# Check if the JAR file has started successfully
+if [ $? -eq 0 ]; then
+  echo "JAR file started successfully"
+else
+  echo "ERROR: Failed to start the JAR file"
+  cat /home/ec2-user/app/output.log
+  exit 1
+fi
