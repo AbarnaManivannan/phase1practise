afb2cb134504970e514925b94849ac5349a5e916
AbarnaManivannan <madhu.vravi@yahoo.com>
2025-05-03 16:47:41 +0530

    Update validate_service.sh

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 echo "Running validate_service.sh"
-pgrep -f hello-world-1.0-SNAPSHOT-jar-with-dependencies.jar > /dev/null
+
+# Check if the Java process for the JAR file is running
+pgrep -f "java -jar hello-world-1.0-SNAPSHOT-jar-with-dependencies.jar" > /dev/null
+if [ $? -eq 0 ]; then
+  echo "Service is running"
+else
+  echo "ERROR: Service is not running"
+  exit 1
+fi
